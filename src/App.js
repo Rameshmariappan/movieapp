fa2910267c970e8aab6275038f511fd0fd72f9e7
Ramesh Mariappan <rameshmariappan.m@gmail.com>
2021-09-08 18:38:07 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
+import React,{ useState,useEffect } from 'react';
+import Movie from './component/Movie';
 
+const FEATURE_API="https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=495969e91d66f40ca12376e10f9c2ac5&page=1";
+const SEARCH_API="https://api.themoviedb.org/3/search/movie?api_key=495969e91d66f40ca12376e10f9c2ac5&query="
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+  const [movies,setMovies] = useState([]);
+  useEffect(() => {
+    fetch(FEATURE_API)
+      .then((res) => res.json())
+      .then((data) => {
+        setMovies(data.results);
+ 
+      })
+  }, []);
 
+
+  return(<>
+    <header className="headers">
+    <input className="search" type="search" placeholder="Search!!!!!!" />
+    </header>
+  <div className="movie-container"> 
+    {movies.length > 0 && movies.map((movie)=>
+    <Movie key={movie.id} {...movie}/>)}
+  </div>
+  </>
+  ); 
+ 
+}; 
 export default App;

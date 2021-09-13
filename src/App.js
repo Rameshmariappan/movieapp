dd984561fb0072cd2096648d36e5c0fb691e870a
Ramesh Mariappan <rameshmariappan.m@gmail.com>
2021-09-13 13:23:56 +0530

    task

--- src/App.js
+++ src/App.js
@@ -4,21 +4,65 @@ const API_KEY = `${process.env.REACT_APP_API_KEY}`;
 const BASE_URL = `${process.env.REACT_APP_BASE}`;
 const FEATURE_API = `${BASE_URL}discover/movie?sort_by=popularity.desc&api_key=${API_KEY}&page=1`;
 const SEARCH_API = `${BASE_URL}search/movie?api_key=${API_KEY}&query=`;
+const UPCOMING_API = `${BASE_URL}movie/upcoming?api_key=${API_KEY}&language=en-US&page=1`;
+const NOWPLAYING_API = `${BASE_URL}movie/now_playing?api_key=${API_KEY}&language=en-US&page=1`;
 function App() {
   const [movies, setMovies] = useState([]);
+  const [searchmovie, setSearchmovie] = useState("");
   useEffect(() => {
-    fetch(FEATURE_API)
+    checkmovie(FEATURE_API);
+  }, []);
+  const checkmovie = (API) => {
+    fetch(API)
       .then((res) => res.json())
       .then((data) => {
         setMovies(data.results);
       });
-  }, []);
+  };
+  const search_handle = (e) => {
+    e.preventDefault();
+    if (searchmovie) {
+      checkmovie(SEARCH_API + searchmovie);
+      setSearchmovie("");
+    }
+  };
+  const searchonchange = (e) => setSearchmovie(e.target.value);
+
+  const upcome = () => {
+    checkmovie(UPCOMING_API);
+  };
+  const popularity = () => {
+    checkmovie(FEATURE_API);
+  };
+  const nowplayed = () => {
+    checkmovie(NOWPLAYING_API);
+  };
 
   return (
     <>
       <header className="headers">
-        <input className="search" type="search" placeholder="Search!!!!!!" />
+        <h2 className="mov">movFlix</h2>
+        <form className="searchbox" onSubmit={search_handle}>
+          <input
+            className="search"
+            type="search"
+            placeholder="Search!!!!!!"
+            onChange={searchonchange}
+            value={searchmovie}
+          />
+        </form>
       </header>
+      <div className="mybutton">
+        <button className="popular" onClick={popularity}>
+          Popular
+        </button>
+        <button className="upcoming" onClick={upcome}>
+          Upcoming
+        </button>
+        <button className="nowplaying" onClick={nowplayed}>
+          Now Playing
+        </button>
+      </div>
       <div className="movie-container">
         {movies.length > 0 &&
           movies.map((movie) => <Movie key={movie.id} {...movie} />)}
--- src/component/Movie.js
+++ src/component/Movie.js
@@ -1,13 +1,23 @@
 import React from "react";
 
 const IMAGE_API = "https://image.tmdb.org/t/p/w500";
-
+const colorofvotes = (vote) => {
+  if (vote > 8) {
+    return "red";
+  } else if (vote > 5) {
+    return "green";
+  } else {
+    return "yellow";
+  }
+};
 const Movie = ({ title, poster_path, overview, vote_average }) => (
   <div className="movie">
     <img src={IMAGE_API + poster_path} alt={title} />
     <div className="movie-name">
       <h3>{title}</h3>
-      <span>IMDB:{vote_average}</span>
+      <span className={`tag ${colorofvotes(vote_average)}`}>
+        {vote_average}
+      </span>
     </div>
     <div className="movie-overview">
       <h2>Movie Overview:</h2>

023337ebce2ece01a8a25b971066baa931a629c3
Ramesh Mariappan <rameshmariappan.m@gmail.com>
2021-09-15 11:26:09 +0530

    paginationadd

--- src/App.js
+++ src/App.js
@@ -1,14 +1,17 @@
 import React, { useState, useEffect } from "react";
+import ReactPaginate from "react-paginate";
 import Movie from "./component/Movie";
 const API_KEY = `${process.env.REACT_APP_API_KEY}`;
 const BASE_URL = `${process.env.REACT_APP_BASE}`;
-const FEATURE_API = `${BASE_URL}discover/movie?sort_by=popularity.desc&api_key=${API_KEY}&page=1`;
+const FEATURE_API = `${BASE_URL}discover/movie?sort_by=popularity.desc&api_key=${API_KEY}&page=`;
 const SEARCH_API = `${BASE_URL}search/movie?api_key=${API_KEY}&query=`;
-const UPCOMING_API = `${BASE_URL}movie/upcoming?api_key=${API_KEY}&language=en-US&page=1`;
-const NOWPLAYING_API = `${BASE_URL}movie/now_playing?api_key=${API_KEY}&language=en-US&page=1`;
+const UPCOMING_API = `${BASE_URL}movie/upcoming?api_key=${API_KEY}&language=en-US&page=`;
+const NOWPLAYING_API = `${BASE_URL}movie/now_playing?api_key=${API_KEY}&language=en-US&page=`;
 function App() {
   const [movies, setMovies] = useState([]);
   const [searchmovie, setSearchmovie] = useState("");
+  const [pageNumber, setPageNumber] = useState(1);
+  const [catagory, setCatagory] = useState("popularity");
   useEffect(() => {
     checkmovie(FEATURE_API);
   }, []);
@@ -23,25 +26,48 @@ function App() {
     e.preventDefault();
     if (searchmovie) {
       checkmovie(SEARCH_API + searchmovie);
+      setCatagory("search");
       setSearchmovie("");
     }
   };
-  const searchonchange = (e) => setSearchmovie(e.target.value);
+  const searchonchange = (e) => {
+    setSearchmovie(e.target.value);
+  };
 
   const upcome = () => {
+    setCatagory("upcoming");
     checkmovie(UPCOMING_API);
   };
   const popularity = () => {
+    setCatagory("popularity");
     checkmovie(FEATURE_API);
   };
   const nowplayed = () => {
+    setCatagory("nowPlaying");
     checkmovie(NOWPLAYING_API);
+  };
+
+  // const pageCount = Math.ceil(movies.length / movieperpage);
+  const pagechange = ({ selected }) => {
+    let finalNo = selected + 1;
+    setPageNumber(finalNo);
+    if (catagory === "popularity") {
+      checkmovie(FEATURE_API + finalNo);
+    } else if (catagory === "upcoming") {
+      checkmovie(UPCOMING_API + finalNo);
+    } else if (catagory === "nowPlaying") {
+      checkmovie(NOWPLAYING_API + finalNo);
+    } else if (catagory === "search") {
+      checkmovie(SEARCH_API + searchmovie + `&page=${finalNo}`);
+    } else {
+      console.log("error");
+    }
   };
 
   return (
     <>
       <header className="headers">
-        <h2 className="mov">movFlix</h2>
+        <h2 className="mov">MovFlix</h2>
         <form className="searchbox" onSubmit={search_handle}>
           <input
             className="search"
@@ -66,6 +92,18 @@ function App() {
       <div className="movie-container">
         {movies.length > 0 &&
           movies.map((movie) => <Movie key={movie.id} {...movie} />)}
+      </div>
+      <div>
+        <ReactPaginate
+          previousLabel={"Previous"}
+          nextLabel={"Next"}
+          onPageChange={pagechange}
+          containerClassName={"paginationBttns"}
+          previousLinkClassName={"previousBttn"}
+          nextLinkClassName={"nextBttn"}
+          disabledClassName={"paginationDisabled"}
+          activeClassName={"paginationActive"}
+        />
       </div>
     </>
   );

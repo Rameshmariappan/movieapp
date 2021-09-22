bf5f1781f887e8131f6880d2cdad3343494d3fdd
Ramesh Mariappan <rameshmariappan.m@gmail.com>
2021-09-22 14:54:06 +0530

    page scroll

--- src/App.js
+++ src/App.js
@@ -1,30 +1,42 @@
 import React, { useState, useEffect } from "react";
-import ReactPaginate from "react-paginate";
+// import ReactPaginate from "react-paginate";
+import InfiniteScroll from "react-infinite-scroll-component";
 import Movie from "./component/Movie";
 import { checkMovie } from "./component/fetchFunction";
 import { FEATURE_API, SEARCH_API, UPCOMING_API, NOWPLAYING_API } from "./Api";
 function App() {
   const [movies, setMovies] = useState([]);
   const [searchmovie, setSearchmovie] = useState("");
-  const [totalpageNumber, setTotalpageNumber] = useState();
-  const [pagenum, setPagenum] = useState();
+  // const [totalpageNumber, setTotalpageNumber] = useState();
+  // const [pagenum, setPagenum] = useState(1);
   const [api, setApi] = useState(FEATURE_API);
   const [currentPage, setCurrentPage] = useState(1);
-  const movieperpage = 20;
+  // const movieperpage = 20;
   useEffect(() => {
-    checkMovie(api, currentPage).then((data) => {
-      setPagenum(data.page - 1);
+    checkMovie(FEATURE_API, currentPage).then((data) => {
+      // setPagenum(data.page - 1);
       setMovies(data.results);
-      setTotalpageNumber(data.total_results);
+      // setTotalpageNumber(data.total_results);
     });
-  }, [api, currentPage]);
+  }, []);
+  const nextScroll = () => {
+    let scrollCount = currentPage + 1;
+    setCurrentPage(scrollCount, currentPage);
+    checkMovie(api, scrollCount).then((data) => {
+      setMovies([...movies, ...data.results]);
+    });
+  };
   const searchHandle = (e) => {
     e.preventDefault();
     if (searchmovie) {
-      setApi(SEARCH_API + searchmovie + `&page=`);
+      setCurrentPage(1);
+      checkMovie(SEARCH_API + searchmovie + `&page=`, 1).then((data) => {
+        setApi(SEARCH_API + searchmovie + `&page=`);
+        setMovies(data.results);
+      });
     }
   };
-  const pageCounts = Math.ceil(totalpageNumber / movieperpage);
+  // const pageCounts = Math.ceil(totalpageNumber / movieperpage);
   return (
     <>
       <header className="headers">
@@ -43,8 +55,11 @@ function App() {
         <button
           className="popular"
           onClick={() => {
-            setApi(FEATURE_API);
             setCurrentPage(1);
+            checkMovie(FEATURE_API, 1).then((data) => {
+              setApi(FEATURE_API);
+              setMovies(data.results);
+            });
           }}
         >
           Popular
@@ -52,8 +67,11 @@ function App() {
         <button
           className="upcoming"
           onClick={() => {
-            setApi(UPCOMING_API);
             setCurrentPage(1);
+            checkMovie(UPCOMING_API, 1).then((data) => {
+              setApi(UPCOMING_API);
+              setMovies(data.results);
+            });
           }}
         >
           Upcoming
@@ -61,8 +79,11 @@ function App() {
         <button
           className="nowplaying"
           onClick={() => {
-            setApi(NOWPLAYING_API);
             setCurrentPage(1);
+            checkMovie(NOWPLAYING_API, 1).then((data) => {
+              setApi(NOWPLAYING_API);
+              setMovies(data.results);
+            });
           }}
         >
           Now Playing
@@ -72,7 +93,15 @@ function App() {
         {movies.length > 0 &&
           movies.map((movie) => <Movie key={movie.id} {...movie} />)}
       </div>
-      <div className="paginate-control">
+      <div>
+        <InfiniteScroll
+          dataLength={movies.length}
+          next={nextScroll}
+          hasMore={true}
+          loader={<h4>Loading...</h4>}
+        />
+      </div>
+      {/* <div className="paginate-control">
         <ReactPaginate
           previousLabel={"Previous"}
           nextLabel={"Next"}
@@ -86,7 +115,7 @@ function App() {
           activeClassName={"paginationActive"}
           forcePage={pagenum}
         />
-      </div>
+      </div> */}
     </>
   );
 }
